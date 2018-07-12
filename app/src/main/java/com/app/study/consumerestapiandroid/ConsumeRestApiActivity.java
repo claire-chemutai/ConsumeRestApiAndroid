9fd1318763cfe73923f3996c6ef5bcfb0593bf60
Claire <clairechemutai12@gmail.com>
2018-07-12 20:39:07 +0300

    Initial commit

---
+++ app/src/main/java/com/app/study/consumerestapiandroid/ConsumeRestApiActivity.java
@@ -0,0 +1,13 @@
+package com.app.study.consumerestapiandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsumeRestApiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consume_rest_api);
+    }
+}
